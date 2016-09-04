f39d1a79c443cb83c845f3bc1798726fce8b3602
Gustavo Pérez Machado <gustperz@gmail.com>
2016-09-04 00:04:45 -0500

    date locale config

---
+++ src/app/config/config.date_locale.js
@@ -0,0 +1,47 @@
+/**
+ * Created by tav0 on 03/09/16.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        // .config(config);
+
+    /** @ngInject */
+    function config($mdDateLocaleProvider) {
+
+        $mdDateLocaleProvider.months = [
+            'Enero', 'Febrero', 'Marzo', 'Abril',
+            'Mayo', 'Junio', 'Julio', 'Agosto',
+            'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+        ];
+        $mdDateLocaleProvider.shortMonths = [
+            'Ene','Feb','Mar','Abr','Mayo','Jun','Jul','Ago','Sept','Oct','Nov','Dic'
+        ];
+        $mdDateLocaleProvider.days = [
+            'Lunes', 'Martes', 'Miércoles', 'Jueves',
+            'Viernes', 'Sábado', 'Domingo'
+        ];
+        $mdDateLocaleProvider.shortDays = [
+            'Lun','Mar','Mié','Jue','Vie','Sáb','Dom'
+        ];
+
+        $mdDateLocaleProvider.parseDate = function(dateString) {
+            var m = moment(dateString, 'L', true);
+            return m.isValid() ? m.toDate() : new Date(NaN);
+        };
+        $mdDateLocaleProvider.formatDate = function(date) {
+            return moment(date).format('L');
+        };
+        $mdDateLocaleProvider.monthHeaderFormatter = function(date) {
+            return $mdDateLocaleProvider.shortMonths[date.getMonth()] + ' ' + date.getFullYear();
+        };
+        $mdDateLocaleProvider.weekNumberFormatter = function(weekNumber) {
+            return 'Semana ' + weekNumber;
+        };
+        $mdDateLocaleProvider.msgCalendar = 'Calendario';
+        $mdDateLocaleProvider.msgOpenCalendar = 'Abrir calendario';
+    }
+})();
